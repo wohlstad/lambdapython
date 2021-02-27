7dc626d17baf6bfe16c0c8e134abfb689112ef00
gilwohl <gil.wohl@gmail.com>
2021-02-27 06:05:13 -0800

    numbers demo

--- demo.py
+++ demo.py
@@ -15,7 +15,8 @@ XOR = lambda x : lambda y : NOT(EQ(x)(y))
 BOOLS = [TRUE, FALSE]
 def bool2Str(b):
     return b("T")("F")
-
+    
+print("boolean logic demo:")
 print("---")
 for b in BOOLS:
     print(bool2Str(b))
@@ -49,3 +50,51 @@ for b1 in BOOLS:
     for b2 in BOOLS:
         b1_xor_b2 = XOR(b1)(b2);
         print(bool2Str(b1) + " XOR " + bool2Str(b2) + " == " + bool2Str(b1_xor_b2))        
+
+
+#-------- numbers
+
+ZERO = lambda f : lambda x : x
+#ONE = lambda f : lambda x : f(x)
+#TWO = lambda f : lambda x : f(f(x))
+SUCC = lambda n : lambda f : lambda x : f(n(f)(x))
+ONE = SUCC(ZERO)
+TWO = SUCC(ONE)
+THREE = SUCC(TWO)
+FOUR = SUCC(THREE)
+
+NUMBERS = [ZERO]
+for i in range(10):
+    last = NUMBERS[len(NUMBERS)-1]
+    NUMBERS.append(SUCC(last))
+
+ADD = lambda n : lambda m : n(SUCC)(m);
+MUL = lambda n : lambda m : lambda f : lambda x : n(m(f))(x)
+#POW = lambda n : lambda m : lambda f: lambda x : (m(n))(f)(x)
+POW = lambda n : lambda m : m(n)
+
+#-------- numbers demo
+
+num2Int = lambda n : n(lambda x : x+1)(0)
+num2Str = lambda n : str(num2Int(n))
+
+print("\n")
+print("numbers demo:")
+print("---")
+
+print(num2Str(ONE))
+print(num2Str(TWO))
+print(num2Str(THREE))
+print(num2Str(FOUR))
+numbersStr = ""
+for n in NUMBERS:
+    numbersStr = numbersStr + num2Str(n) + ", "
+print(numbersStr)
+print("---") 
+n1 = NUMBERS[3]
+n2 = NUMBERS[4]
+print(num2Str(n1) + " + " + num2Str(n2) + " == " + num2Str(ADD(n1)(n2))); 
+print(num2Str(n1) + " * " + num2Str(n2) + " == " + num2Str(MUL(n1)(n2))); 
+print(num2Str(n1) + " ^ " + num2Str(n2) + " == " + num2Str(POW(n1)(n2))); 
+print("---") 
+
